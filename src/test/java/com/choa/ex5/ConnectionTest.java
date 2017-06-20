91c85181aae771094ed9dfea2cd0768544f379e7
user2-PC\user2 <ula03@naver.com>
2017-06-20 21:23:14 +0900

    1. pom.xml 수정 2. root-context.xml 수정 3. 4. resources /mappers 디렉터리 생성 5. 6. test Connection

---
+++ src/test/java/com/choa/ex5/ConnectionTest.java
@@ -0,0 +1,20 @@
+package com.choa.ex5;
+
+import static org.junit.Assert.*;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ConnectionTest extends TestUnit{
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Test
+	public void test() {
+		assertNotNull(sqlSession);
+		
+	}
+
+}
